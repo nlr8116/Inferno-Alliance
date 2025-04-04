5a9d4ba0f588ddf57c8e7f4e815b80b9dd7ef115
Nico <nicorelle351@gmail.com>
2025-04-04 14:19:06 -0500

    4/4/25

---
+++ 01 More on Data Structure/08 Dictionary Comp/Example.py
@@ -0,0 +1,21 @@
+
+
+values = { i : i**2 for i in range(10)}
+print(values)
+
+values = {i : "hello" for i in range(10)}
+print(values)
+
+values = {i: i**3 for i in range (20) if i%2 == 0}
+print(values)
+
+names = ["William","Ashish","Joseph","Kim","Aayusha"]
+result = { name: len(name) for name in names}
+print(result)
+
+ages = [33000, 21, 13, 26, 900]
+result = {names[i] : ages[i] for i in range(len(names))}
+print(result)
+
+result = {names[i] : ages[i] for i in range(len(names)) if ages[i] >= 21}
+print(result)
